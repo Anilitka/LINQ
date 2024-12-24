ceb11742eb1b79de6de97862fda8e328829ff1b2
Anna Berishvili <berishviliannaa@gmail.com>
2024-12-24 10:59:18 +0400

    adding files

---
+++ Linq/GroupingData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Linq.DataSources;
+using Linq.EqualityComparers;
+
+namespace Linq
+{
+    /// <summary>
+    /// Considers how to group elements of sequences into buckets ( `group by` and `into` keywords, methods GroupBy, GroupJoin ) in LINQ queries.
+    /// Grouping definition: <see cref="IEnumerable{TSource}"/> → <see cref="IEnumerable{IGrouping{TKey,TElement}}"/> 
+    /// Grouping refers to the operation of putting data into groups so that the elements in each group share a common attribute.
+    /// </summary>
+    public static class GroupingData
+    {
+        /// <summary>
+        /// Partitions a list of words by their first letter ans sorts by it.
+        /// </summary>
+        /// <returns>Sorted by key (first letter) sequence of words grouped by first letter.</returns>
+        public static IEnumerable<IGrouping<char, string>> GroupByProperty()
+        {
+            string[] words = { "blueberry", "chimpanzee", "abacus", "banana", "apple", "cheese" };
+
+            return words.GroupBy(x => x[0]).OrderBy(w => w.Key);
+        }
+
+        /// <summary>
+        /// Groups elements on the remainder of an integer when dividing it by 5.
+        /// </summary>
+        /// <returns>The sequence of pairs: the remainder of an integer when dividing it by 5 and the numbers with a given remainder.</returns>
+        public static IEnumerable<(int remainder, IEnumerable<int> numbers)> Grouping()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            return numbers.GroupBy(n => n % 5).Select(num => (num.Key, num.AsEnumerable())); ;
+        }
+
+        /// <summary>
+        /// Partitions a list of products by category.
+        /// </summary>
+        /// <returns>The sequences of products grouped by categories in which the number of products is less than or equal to 7.</returns>
+        public static IEnumerable<(string category, IEnumerable<string> productsName)> GroupByCategory()
+        {
+            List<Product> products = Products.ProductList;
+
+            return products.GroupBy(p => p.Category)
+                .Where(p => p.Count() <= 7)
+                .Select(p => (p.Key, p.Select(p => p.ProductName)));
+        }
+
+        /// <summary>
+        /// Partitions a list of words by custom comparer <see cref="AnagramEqualityComparer"/>.
+        /// </summary>
+        /// <returns>The sequences of words grouped by anagram comparer.</returns>
+        public static IEnumerable<IGrouping<string, string>> GroupByCustomComparer()
+        {
+            string[] anagrams =
+                {"from   ", "  mane", " salt", " earn ", "name   ", "  last   ", " near ", " form  ", "mean"};
+
+
+            var groupedAnagrams = anagrams
+                 .GroupBy(word => new string(word.Where(c => !char.IsWhiteSpace(c)).ToArray()), new AnagramEqualityComparer())
+                 .Select(group => new
+                 {
+                     Key = group.First().Trim(),
+                     Words = group
+                 })
+                 .Select(group => group.Words);
+
+            return groupedAnagrams;
+        }
+
+        /// <summary>
+        /// Partitions a list of words by custom comparer <see cref="AnagramEqualityComparer"/>.
+        /// </summary>
+        /// <returns>The sequences of words in upper case grouped by anagram comparer.</returns>
+        public static IEnumerable<IGrouping<string, string>> NestedGroupByCustom()
+        {
+            string[] anagrams =
+                {"from   ", "  mane", " salt", " earn ", "name   ", "  last   ", " near ", " form  ", "mean"};
+
+            throw new NotImplementedException();
+        }
+    }
+}
